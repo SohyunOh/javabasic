e8e82f6b7ff2c697618b39dcea6563f44be01679
ohso <73691966+SohyunOh@users.noreply.github.com>
2021-03-24 18:32:10 +0900

    print  "Hello world " 출력문

--- day01/src/day01/HelloJava.java
+++ day01/src/day01/HelloJava.java
@@ -3,8 +3,8 @@ package day01;
 public class HelloJava {
 
 	public static void main(String[] args) {
-		
-		//  \n은 줄바꿈 명령문입니다
+		 
+		// \n은 줄바꿈 명령문입니다
 		System.out.print("Hello world?\n");
 		System.out.print("안녕하세요\n");
 		System.out.print("1\n");
