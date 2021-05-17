5a5e246e279beaae34b09e9e07dd79be79f2bf86
ohso <73691966+SohyunOh@users.noreply.github.com>
2021-05-17 23:54:19 +0900

    PenMain(펜_메인_틀레스)

--- day08/src/day08/PenMain.java
+++ day08/src/day08/PenMain.java
@@ -5,13 +5,13 @@ public class PenMain {
 	public static void main(String[] args) {
 		
 		//Pen의 기능을 사용하려면 설계용클래스를 객체로 생성해야 합니다.
-		Pen black = new Pen();
+		Pen black = new Pen(); // 검정 팬 객체 생성 
 		black.color = "black";
 		black.price = 1000;
 		black.company = "모나미";
 		black.write();
 		
-		Pen red = new Pen();
+		Pen red = new Pen(); // 빨간 펜 객체 생성
 		red.color = "red";
 		red.price = 2000;
 		red.company = "하이테크";
@@ -21,6 +21,6 @@ public class PenMain {
 		black.info();
 		red.info();
 		
-		
+		 
 	}
 }
