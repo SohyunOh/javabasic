04d5a66c96fa32cad7037c70d2d8a288ee59cf25
ohso <73691966+SohyunOh@users.noreply.github.com>
2021-06-07 17:48:02 +0900

    Student

--- day09/src/overriding/basic2/Student.java
+++ day09/src/overriding/basic2/Student.java
@@ -3,7 +3,7 @@ package overriding.basic2;
 public class Student extends Person {
 
 	String studentId;
-	//info메서드 오버라이딩 (학번 정보를 포함해서 리턴)
+	//info메서드 오버라이딩 (학번 정보를 포함해서 리턴) 
 	
 	String info() {
 		return "이름:" + name + ", 나이:" + age + ", 학번:" + studentId;
