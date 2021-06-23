8226ff4814695f039dbcee5a5931cd04f220c62d
ohso <73691966+SohyunOh@users.noreply.github.com>
2021-06-23 23:00:05 +0900

    bad_mainClass

--- day10/src/encap/bad/MainClass.java
+++ day10/src/encap/bad/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		//멤버변수를 public으로 선언하면 잘못된 값이 저장될 수도 있음
+		
 		MyDate me = new MyDate();
 		me.year = 2020;
 		me.month = 13;
