7007989b8885d5ba4abcfc493efb8cd72e191634
ohso <73691966+SohyunOh@users.noreply.github.com>
2021-06-30 23:35:16 +0900

    final변수_ MainCalss

--- day11/src/final_/field/MainCalss.java
+++ day11/src/final_/field/MainCalss.java
@@ -7,7 +7,7 @@ public class MainCalss {
 		Person park = new Person("123123", "박한국");
 		Person kim  = new Person("456465", "박대한");
 		
-		kim.name = "김대만";
+		kim.name = "김대만"; 
 //		kim.nation = "미국";
 		
 		
