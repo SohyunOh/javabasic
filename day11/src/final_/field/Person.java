beb1a47e71164bd69544d7f1f71a66db9a173ab2
ohso <73691966+SohyunOh@users.noreply.github.com>
2021-06-30 23:33:44 +0900

    final변수_Person

--- day11/src/final_/field/Person.java
+++ day11/src/final_/field/Person.java
@@ -4,12 +4,12 @@ public class Person {
 	
 	/*
 	 * final 변수는 값이 대입되면 변경 할 수 없기; 때문에
-	 * 직접 초기화 하거나, 생성자를 통해서 잔드시 초기화 해야한다.
+	 * 직접 초기화 하거나, 생성자를 통해서 반드시 초기화 해야한다.
 	 * 
 	 */
 	
 	public final String nation = "대한민국";
-	public final String ssn; //주민번호
+	public final String ssn; //주민번호 
 	public String name;
 
 	public Person (String ssn, String name) {
