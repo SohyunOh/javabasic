21092c6511d3c1c56d61b7b25579b9f36874f09e
ohso <73691966+SohyunOh@users.noreply.github.com>
2021-07-05 23:48:05 +0900

    Const

--- day11/src/constant/Const.java
+++ day11/src/constant/Const.java
@@ -2,7 +2,7 @@ package constant;
 
 public class Const {
 	
-	//static final 상수 표현
+	//static final 상수(수식이 변하지 않는 값) 표현 
 	
 	public static final double PI = 3.141592;
 	public static final int O2 = 32;
